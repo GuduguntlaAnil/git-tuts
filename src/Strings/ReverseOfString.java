178b2e4205375d481d8a17dbb5b43828bdb94c57
Guduguntla Anil <guduguntlaanil@gmail.com>
2024-09-01 16:55:10 +0530

    commit

---
+++ src/Strings/ReverseOfString.java
@@ -0,0 +1,162 @@
+package Strings;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseOfString {
+	public static void main(String [] args) {
+		System.out.println("Hello world");
+		}
+		
+		
+//		String str2=sc.nextLine();
+//		str1=str1+str2;
+//		str2=str1.substring(0,str1.length()-str2.length());
+//		str1=str1.substring(str2.length());
+//		System.out.println(str1+ " "+str2);
+		
+//		String noSpace="";
+//		for(int i=0;i<str1.length();i++) {
+//			if(str1.charAt(i)==' ') {
+//				noSpace=noSpace+"-";
+//			}
+//			else {
+//				noSpace=noSpace+str1.charAt(i);
+//			}
+//			
+//		}
+//		System.out.println(noSpace.trim());
+//		
+		
+//		String toggle="";
+//		
+//		for(int i=0;i<str1.length();i++) {
+//			if(str1.charAt(i)>=97 && str1.charAt(i)<=122) {
+//		int c=str1.charAt(i)-32;
+//			toggle=toggle+(char) c;
+//			}
+//			else if(str1.charAt(i)>=65 && str1.charAt(i)<=90){
+//				int c=str1.charAt(i)+32;
+//				toggle=toggle+(char) c;
+//			}
+//			else  {
+//				toggle=toggle+str1.charAt(i);
+//			}
+//			
+//		}
+//	System.out.println(toggle);
+//		
+		
+//		String arr[]=str1.split(" ");
+//	for(int i=0;i<arr.length;i++) {
+//		StringBuffer s=new StringBuffer(arr[i]);
+//		s.reverse();
+//		System.out.print(s+" ");
+//	}
+
+		
+		
+//		String arr[]=str1.split(" ");
+//		for(int i=0;i<arr.length;i++) {
+//			try {
+//				arr[i]=Integer.parseInt(arr[i])+"";
+//			}
+//			catch(Exception e) {
+//				
+//				arr[i]=-1+"";
+//			}
+//			
+//		}
+//		
+//		
+//		for(int i=0;i<arr.length;i++) {
+//		System.out.print(arr[i]+" ");
+//		
+//		}
+			
+		
+	
+//		String[] arr=str1.split(" ");
+//		int max=arr[0].length();
+//		int min=arr[0].length();
+//		String maxw=arr[0];
+//		String minw=arr[0];
+//		for(int i=1;i<arr.length;i++) {
+//			if(arr[i].length()>max) {
+//				max=arr[i].length();
+//				maxw=arr[i];
+//			}
+//			if(arr[i].length()<min) {
+//				min=arr[i].length();
+//				minw=arr[i];
+//				}
+//		
+//			}
+//		System.out.println(maxw+" "+minw);
+//		
+//		
+//		System.out.println(maxw+" "+max);
+//		char c=sc.next().charAt(0);
+//		str1=str1+c;
+//		
+		
+//		StringBuilder s=new StringBuilder(str1);
+//		s.insert(2,'a');
+//		System.out.println(s);
+//	
+//		String str2=sc.nextLine();
+//		if(str1.length()==str2.length()) {
+//			char[] arr1=str1.toCharArray();
+//			char[] arr2=str2.toCharArray();
+//			Arrays.sort(arr1);
+//			Arrays.sort(arr2);
+//			if(Arrays.equals(arr1, arr2)) {
+//				System.out.println("anagrams");
+//			}
+//			else {
+//				System.out.println("not an anagrams");
+//			}
+			
+			
+	//				}
+//		String str2=sc.nextLine();
+//		int insert=sc.nextInt();
+//		String insertstr="";
+//		for(int i=0;i<str1.length();i++) {
+//			insertstr=insertstr+str1.charAt(i);
+//			if(i==insert) {
+//				insertstr=insertstr+str2;
+//			}
+//		}
+//		System.out.println(insertstr);
+//		
+//		String[] arr=str.split(" ");
+//		for(int i=0;i<arr.length;i++) {
+//			if(arr[i].length()%2==0) {
+//				System.out.println(arr[i]);
+//			}
+//			
+//		}
+//		
+//		String revstr="";
+//		StringBuilder s=new StringBuilder(str);
+//		System.out.println(s.reverse());
+//		for(int i=str.length()-1;i>=0;i--) {
+//			revstr=revstr+str.charAt(i);
+//			
+//		}
+//		String revstr="";
+//		for(int i=0;i<str1.length();i++) {
+//			revstr=str1.charAt(i)+revstr;
+//		}
+//		System.out.println(revstr.equals(str1));
+//		if(revstr.equals(str1)) {
+//			System.out.println("palilndrome");
+//	}
+//		else {
+//			System.out.println("not palilndrome");
+//		}
+	}
+
+
