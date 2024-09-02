81dc37264a525ccc2143a218dc2ac9544558358e
GuduguntlaAnil <115879436+GuduguntlaAnil@users.noreply.github.com>
2024-09-02 09:21:32 +0530

    Update ReverseOfString.java

--- src/Strings/ReverseOfString.java
+++ src/Strings/ReverseOfString.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class ReverseOfString {
 	public static void main(String [] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world guys");
 		}
 		
 		
